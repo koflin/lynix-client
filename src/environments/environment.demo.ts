c96ba25961e1c456cdbb0a20680ac2c43793e790
Colin Pfingstl <colin.pfingstl@hotmail.ch>
2021-05-11 23:59:27 +0200

    Demo conf

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiHost: 'https://api.demo.lynix.ch/v0/',
+  gatewayHost: 'wss://gateway.demo.lynix.ch',
+  domain: 'lynix.ch',
+  clientHost: 'https://demo.lynix.ch/'
+};
