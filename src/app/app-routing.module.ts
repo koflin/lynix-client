b29246bfd78555a70b099a315aa725b80437bb65
Colin Pfingstl <colin.pfingstl@hotmail.ch>
2021-05-23 00:24:06 +0200

    Now running

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthGuard } from './auth/auth.guard';
 import { ActivationComponent } from './views/activation/activation.component';
 import { LoginComponent } from './views/login/login.component';
 
@@ -12,13 +11,13 @@ const routes: Routes = [
   { path: 'activation/:id', component: ActivationComponent },
 
   //{ path: 'home',  canActivate: [AuthGuard], loadChildren: () => import('./home/home.module').then(m => m.HomeModule )},
-  { path: 'home',  canActivate: [AuthGuard], loadChildren: './home/home.module#HomeModule'},
+  { path: 'home', loadChildren: './home/home.module#HomeModule'},
 
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'corrected' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
